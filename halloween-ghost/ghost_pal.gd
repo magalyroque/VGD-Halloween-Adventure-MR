73569f3e9951b6427df813cee6856dbfb958a4dd
magalyroque <roquemagaly76@gmail.com>
2024-10-31 12:15:39 -0500

    Modified Halloween Game

--- halloween-ghost/ghost_pal.gd
+++ halloween-ghost/ghost_pal.gd
@@ -1,25 +1,57 @@
 extends CharacterBody2D
 
+@export var SPEED: int = 150
+@export var JUMP_FORCE: int = 255
+@export var GRAVITY : int = 900
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func _physics_process(delta):
+	
+	
+	var direction = Input.get_axis("Left","Right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		
+		velocity.x = SPEED * direction
+		
+		if is_on_floor():
+			
+			$AnimatedSprite2D.play("Run")
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+		velocity.x = 0
+		
+		if is_on_floor():
+			
+			$AnimatedSprite2D.play("Idle")
+	
+	# Rotate
+	
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h = true
+		
+	
+	# Gravity
+	
+	if not is_on_floor():
+		
+		velocity.y += GRAVITY * delta
+		
+		if velocity.y > 0:
+			
+			$AnimatedSprite2D.play("Fall")
+	
+	# Jump
+	
+	if is_on_floor():
+		
+		if Input.is_action_just_pressed("Jump"):
+			
+			velocity.y -= JUMP_FORCE
+			$AnimatedSprite2D.play("Jump")
+	
+	
 	move_and_slide()
+	
